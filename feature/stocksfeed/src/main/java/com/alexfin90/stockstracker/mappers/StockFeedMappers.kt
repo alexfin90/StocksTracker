58b9b685026af6d3c8c3ccdcbf86af42d44fa9f0
Alessandro Finocchiaro <afinocchiaro@technogym.com>
2026-04-09 17:58:04 +0200

    feat: add UiStock model and mapping logic to stocksfeed module
    - Create `UiStock` immutable data class for UI representation.
    - Implement `toUiModel` extension function to map `Stock` entities to `UiStock`.

---
+++ feature/stocksfeed/src/main/java/com/alexfin90/stockstracker/mappers/StockFeedMappers.kt
@@ -0,0 +1,11 @@
+package com.alexfin90.stockstracker.mappers
+
+import com.alexfin90.stockstracker.entities.Stock
+import com.alexfin90.stockstracker.uimodels.UiStock
+
+fun Stock.toUiModel() = UiStock(
+    symbol = this.symbol,
+    name = this.name,
+    priceUsd = this.priceUsd,
+    isIncrease = this.isIncrease == true
+)
