bcea187acead83be7b649688ac95c647dfd9830b
Alessandro Finocchiaro <afinocchiaro@technogym.com>
2026-04-09 19:21:03 +0200

    feat: integrate StartConnectionUseCase and StopConnectionUseCase in MainViewModel for connection management

--- app/src/main/java/com/alexfin90/stockstracker/StocksTrackerActivityViewModel.kt
+++ app/src/main/java/com/alexfin90/stockstracker/StocksTrackerActivityViewModel.kt
@@ -3,6 +3,8 @@ package com.alexfin90.stockstracker
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.alexfin90.stockstracker.usecases.ObserveConnectionUseCase
+import com.alexfin90.stockstracker.usecases.StartConnectionUseCase
+import com.alexfin90.stockstracker.usecases.StopConnectionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
@@ -11,6 +13,8 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     observeConnectionUseCase: ObserveConnectionUseCase,
+    private val startConnectionUseCase: StartConnectionUseCase,
+    private val stopConnectionUseCase: StopConnectionUseCase,
 ) : ViewModel(), StocksTrackerActivityViewModelContract {
 
     override val connectionActive = observeConnectionUseCase().stateIn(
@@ -20,5 +24,10 @@ class MainViewModel @Inject constructor(
     )
 
     override fun onToggleConnection() {
+        if (connectionActive.value) {
+            stopConnectionUseCase()
+        } else {
+            startConnectionUseCase()
+        }
     }
 }
