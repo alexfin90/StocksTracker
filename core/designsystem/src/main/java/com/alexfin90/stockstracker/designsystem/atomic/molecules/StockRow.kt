dc175530377947c1867e6d20dec13c3a34629ce8
Alessandro Finocchiaro <afinocchiaro@technogym.com>
2026-04-10 19:15:59 +0200

    feat: modifier extension for flash price

--- core/designsystem/src/main/java/com/alexfin90/stockstracker/designsystem/atomic/molecules/StockRow.kt
+++ core/designsystem/src/main/java/com/alexfin90/stockstracker/designsystem/atomic/molecules/StockRow.kt
@@ -1,5 +1,8 @@
 package com.alexfin90.stockstracker.designsystem.atomic.molecules
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,8 +16,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -22,6 +31,7 @@ import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.alexfin90.stockstracker.designsystem.atomic.atoms.icons.DownPriceIcon
 import com.alexfin90.stockstracker.designsystem.atomic.atoms.icons.UpPriceIcon
+import kotlinx.coroutines.delay
 
 @Composable
 fun StockRow(
@@ -42,8 +52,18 @@ fun StockRow(
             Text(text = stock.symbol, style = MaterialTheme.typography.titleMedium)
             Text(text = stock.name, style = MaterialTheme.typography.bodySmall)
         }
-        Row(verticalAlignment = Alignment.CenterVertically) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Text(
+                modifier = Modifier.animatedFlashBackground(
+                    triggerKey = stock.priceUsd,
+                    flashColor = if (stock.isUp) {
+                        Color.Green.copy(alpha = 0.5f)
+                    } else {
+                        Color.Red.copy(alpha = 0.5f)
+                    }
+                ),
                 text = "$%.2f".format(stock.priceUsd),
                 style = MaterialTheme.typography.titleMedium,
             )
@@ -77,3 +97,27 @@ private fun StockRowPreview(
     )
 }
 
+
+fun Modifier.animatedFlashBackground(
+    triggerKey: Any?,
+    flashColor: Color,
+    animationDurationMillis: Int = 1000,
+) = composed {
+    var targetColor by remember { mutableStateOf(Color.Transparent) }
+
+    LaunchedEffect(triggerKey) {
+        if (triggerKey != null) {
+            targetColor = flashColor
+            delay(animationDurationMillis.toLong())
+            targetColor = Color.Transparent
+        }
+    }
+
+    val animatedColor by animateColorAsState(
+        targetValue = targetColor,
+        animationSpec = tween(durationMillis = animationDurationMillis),
+        label = "flashBackgroundColor"
+    )
+
+    background(animatedColor)
+}
